7b14997108578f50b28ce7000ef85910d7a35e02
hendra lw <hndralw@gmail.com>
2023-02-23 21:47:40 +0700

    [Enhancement] change method patch to method post

--- src/main/java/com/procurement/apps/controller/ProcurementController.java
+++ src/main/java/com/procurement/apps/controller/ProcurementController.java
@@ -45,7 +45,7 @@ public class ProcurementController {
         return ResponseEntity.status(result.getCode()).body(result);
     }
 
-    @PatchMapping("/v1/procurements")
+    @PostMapping("/v1/procurements")
     public ResponseEntity updateProcurement(@RequestBody ProcurementRequest request){
         var result = procurementService.updateProcurement(request);
         return ResponseEntity.status(result.getCode()).body(result);
